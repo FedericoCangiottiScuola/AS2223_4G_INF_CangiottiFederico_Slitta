fa1548501d87cb500cf26a92402323dec85a606e
Federico Cangiotti <federico.cangiotti05@gmail.com>
2023-01-09 11:28:37 +0100

    feat: Implementa la classe Slitta

--- SlittaDLL/Slitta.cs
+++ SlittaDLL/Slitta.cs
@@ -2,5 +2,36 @@
 {
     internal class Slitta
     {
+        double maxPeso;
+        double maxVolume;
+        List<Regalo> regali;
+
+        public Slitta(double maxPeso, double maxVolume)
+        {
+            this.maxPeso = maxPeso;
+            this.maxVolume = maxVolume;
+            regali = new List<Regalo> { };
+        }
+
+        public bool AggiungiRegalo(Regalo inputRegalo)
+        {
+            double peso = 0;
+            double volume = 0;
+
+            foreach (Regalo regalo in regali)
+            {
+                peso += regalo.Peso;
+                volume += regalo.Volume;
+            }
+            peso += inputRegalo.Peso;
+            volume += inputRegalo.Volume;
+
+            if (peso > maxPeso || volume > maxVolume)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
