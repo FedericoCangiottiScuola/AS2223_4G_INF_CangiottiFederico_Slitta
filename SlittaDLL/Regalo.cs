5d0e13047538b8856368926454a34bfee54e09d5
Federico Cangiotti <federico.cangiotti05@gmail.com>
2023-01-09 11:14:30 +0100

    feat: Implementa la classe Regalo

--- SlittaDLL/Regalo.cs
+++ SlittaDLL/Regalo.cs
@@ -2,5 +2,17 @@
 {
     internal class Regalo
     {
+        public double Peso { get; }
+        public double Volume { get; }
+        public string Colore { get; }
+        public string Descrizione { get; }
+
+        public Regalo(double peso, double volume, string colore, string descrizione)
+        {
+            Peso = peso;
+            Volume = volume;
+            Colore = colore;
+            Descrizione = descrizione;
+        }
     }
 }
