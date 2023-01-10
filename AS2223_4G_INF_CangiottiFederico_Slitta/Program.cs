e568f9dd6108a528124ef09fb6877045d0cf4281
Federico Cangiotti <federico.cangiotti05@gmail.com>
2023-01-10 08:03:45 +0100

    refactor: Modifica dati di ingresso Main

--- AS2223_4G_INF_CangiottiFederico_Slitta/Program.cs
+++ AS2223_4G_INF_CangiottiFederico_Slitta/Program.cs
@@ -6,7 +6,7 @@ namespace AS2223_4G_INF_CangiottiFederico_Slitta
     {
         static void Main(string[] args)
         {
-            Slitta slitta = new Slitta(400.60, 140.67);
+            Slitta slitta = new Slitta(100.60, 140.67);
             Regalo computer = new Regalo(23.4, 12.5, "nero", "computer");
             Regalo libro = new Regalo(40.6, 25.8, "giallo", "libro");
             Regalo zaino = new Regalo(34.5, 70.6, "rosso", "zaino");
