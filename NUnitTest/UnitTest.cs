32ad65452dec9a9ce49cd75a263a928c459665b9
Federico Cangiotti <federico.cangiotti05@gmail.com>
2023-01-10 08:16:15 +0100

    feat: Implementa il test del metodo AggiungiRegalo

--- NUnitTest/UnitTest.cs
+++ NUnitTest/UnitTest.cs
@@ -1,3 +1,5 @@
+using SlittaDLL;
+
 namespace NUnitTest
 {
     public class Tests
@@ -8,8 +10,17 @@ namespace NUnitTest
         }
 
         [Test]
-        public void Test1()
+        public void Test_AggiungiRegalo()
         {
+            Slitta slitta = new Slitta(150.56, 236.67);
+            Regalo computer = new Regalo(100.56, 100.07, "nero", "computer");
+            Regalo libro = new Regalo(25.5, 100.60, "giallo", "libro");
+            Regalo zaino = new Regalo(24.5, 36, "rosso", "zaino");
+
+            Assert.That(slitta.AggiungiRegalo(computer), Is.EqualTo(true));
+            Assert.That(slitta.AggiungiRegalo(libro), Is.EqualTo(true));
+            Assert.That(slitta.AggiungiRegalo(zaino), Is.EqualTo(true));
+            Assert.That(slitta.AggiungiRegalo(zaino), Is.EqualTo(false));
         }
     }
 }
